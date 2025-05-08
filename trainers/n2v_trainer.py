245c9154754f50eaa55ef819bdd97628fba9bfef
Calvin <cl-117@outlook.com>
2025-05-09 00:30:13 +1000

    fixed n2v trainer

--- trainers/n2v_trainer.py
+++ trainers/n2v_trainer.py
@@ -1,13 +1,13 @@
 from schemas.components.train import train
 from utils.config import get_config
 
-def train_n2v(config_path=None, ssm=False):
+def train_n2v(config_path=None, ssm=False, override_config=None):
     
     if config_path is None:
         # Default path to the configuration file
         config_path = r"C:\Users\CL-11\OneDrive\Repos\OCTDenoisingFinal\configs\n2_config.yaml"
 
-    config = get_config(config_path)
+    config = get_config(config_path, override_config)
 
     train(config, "n2v", ssm)
 
