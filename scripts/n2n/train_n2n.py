f06193189fc706c915a4bcf3ce135b6406066fb8
Calvin <cl-117@outlook.com>
2025-05-29 22:03:16 +1000

    Uncomment SSM training line in train_n2n.py to enable training with SSM configuration

--- scripts/n2n/train_n2n.py
+++ scripts/n2n/train_n2n.py
@@ -28,7 +28,7 @@ def main():
     set_seed(42)
     train_n2(config_path=N2_PATH, schema=schema, ssm=False, override_config=override_dict)
     set_seed(42)
-    #train_n2(config_path=N2_PATH, schema=schema, ssm=True, override_config=override_dict)
+    train_n2(config_path=N2_PATH, schema=schema, ssm=True, override_config=override_dict)
 
 if __name__ == "__main__":
     main()
